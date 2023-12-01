c00fd02e7ff6bd8bc967510bde7f6e623969ae10
shioncha <57836246+shioncha@users.noreply.github.com>
2023-12-02 00:32:13 +0900

    Add：Footer

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,9 @@
+import styles from '@/components/footer/footer.module.css'
+
+export function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <p>&copy; 2023 mixne</p>
+        </footer>
+    )
+}
