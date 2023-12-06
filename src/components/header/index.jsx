61818482ae40eb88e0f6018ee8594e985cadc1ca
shioncha <57836246+shioncha@users.noreply.github.com>
2023-12-06 19:21:11 +0900

    Fix：ヘッダーを修正

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,15 +4,13 @@ import styles from '@/components/header/header.module.css'
 export function Header() {
   return (
     <header className={styles.header}>
-      <>
-        <Link href="/"><h1>mixne.net</h1></Link>
-        <li className={styles.container}>
-          <ul><Link href="/">Home</Link></ul>
-          <ul><Link href="/about">About</Link></ul>
-          <ul><Link href="/blog/next-js-app-dir-tutorial0">next-js-app-dir-tutorial0</Link></ul>
-          <ul><Link href="/blog/next-js-app-dir-tutorial1">next-js-app-dir-tutorial1</Link></ul>
-        </li>
-      </>
+      <Link href="/"><h1>mixne.net</h1></Link>
+      <ul className={styles.container}>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/about">About</Link></li>
+        <li><Link href="/blog/next-js-app-dir-tutorial0">next-js-app-dir-tutorial0</Link></li>
+        <li><Link href="/blog/next-js-app-dir-tutorial1">next-js-app-dir-tutorial1</Link></li>
+      </ul>
     </header>
   )
 }
