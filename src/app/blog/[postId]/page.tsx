634a9115bc46525df155ea40493ca2e5913e92d0
shioncha <57836246+shioncha@users.noreply.github.com>
2023-12-10 17:36:54 +0900

    Change：generate blog pages from microCMS

---
+++ src/app/blog/[postId]/page.tsx
@@ -0,0 +1,46 @@
+import { notFound } from "next/navigation";
+import parse from "html-react-parser";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+import { getDetail, getList } from "@/libs/microcms";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+export async function generateStaticParams() {
+    const { contents } = await getList();
+
+    const paths = contents.map((post) => {
+        return {
+            postId: post.id,
+        };
+    });
+
+    return [...paths];
+}
+
+export default async function StaticDetailPage({
+    params: { postId },
+}: {
+    params: { postId: string };
+}) {
+    const post = await getDetail(postId);
+
+ // ページの生成された時間を取得
+    const time = new Date().toLocaleString();
+
+    if (!post) {
+        notFound();
+    }
+
+    return (
+        <div>
+            <h1>{post.title}</h1>
+            <p>公開：{dayjs.utc(post.publishedAt).tz('Asia/Tokyo').format('YYYY年MM月DD日')}</p>
+            <p>カテゴリー：{post.category.name}</p>
+            <h2>{time}</h2>
+            <div>{parse(post.content)}</div>
+        </div>
+    );
+}
