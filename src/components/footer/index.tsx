967a81dcf93f9604786146994091c09b566d61f6
shioncha <57836246+shioncha@users.noreply.github.com>
2023-12-17 11:01:43 +0900

    Add navigation links to footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,8 +1,16 @@
 import styles from '@/components/footer/footer.module.css'
+import Link from 'next/link'
 
 export function Footer() {
     return (
         <footer className={styles.footer}>
+            <div className={styles.navigation}>
+                <ul>
+                    <li><Link href="/about">このサイトについて</Link></li>
+                    <li><Link href="/contact">お問い合わせ</Link></li>
+                    <li><Link href="/privacy">プライバシーポリシー</Link></li>
+                </ul>
+            </div>
             <p>&copy; 2023 mixne</p>
         </footer>
     )
